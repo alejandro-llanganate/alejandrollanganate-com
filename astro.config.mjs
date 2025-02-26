49cb96c64135838c0d82eac48bd6ee9deba2cac9
Alejandro Llanganate <alejandro.llanganate@owasp.org>
2025-02-26 16:18:55 -0500

    fix: page:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,31 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.alejandrollanganate.com',
   base: '/',
   output: 'static',
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        // Incluye los sets de iconos que estés usando
+        'mdi': ['*'],
+        'fa': ['*'],
+        // Agrega otros sets según necesites
+      }
+    })
+  ],
   vite: {
     build: {
       charset: 'utf8',
     },
-    ssr: {
-      noExternal: ['three'],
-    },
-    optimizeDeps: {
-      include: ['three'],
-    },
-  },
-  integrations: [
-    tailwind(),
-  ],
+    resolve: {
+      alias: {
+        'three': 'three/build/three.module.js'
+      }
+    }
+  }
 });
